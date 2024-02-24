827014e26f2da2a5a811eeb7ee27da86164cb1c8
“Follograph” <cifiyang@outlook.com>
2024-02-24 20:43:40 +0800

    mark2

--- simple_case.py
+++ simple_case.py
@@ -6,35 +6,37 @@
 # distribution of this software and related documentation without an express
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
 
-import argparse
+import argparse     #解析命令行参数
 from omni.isaac.kit import SimulationApp
-import numpy as np
-import omni
+import numpy as np      #数值计算
+import omni             #访问omniverse的工具
 
-# This sample loads a usd stage and starts simulation
+# This sample loads a usd stage and starts simulation，定义CONFIG词典
 CONFIG = {"width": 1280, "height": 720, "sync_loads": True, "headless": False, "renderer": "RayTracedLighting"}
 
 
 # Set up command line arguments
 parser = argparse.ArgumentParser("Usd Load sample")
 parser.add_argument("--headless", default=False, action="store_true", help="Run stage headless")
+#添加--headless参数
 
-args, unknown = parser.parse_known_args()
+args, unknown = parser.parse_known_args()       #用于处理额外参数
 # Start the omniverse application
 CONFIG["headless"] = args.headless
 simulation_app = SimulationApp(launch_config=CONFIG)
 
-from omni.isaac.core import World
-from omni.isaac.core.robots import Robot
-from omni.isaac.core.utils.types import ArticulationAction
+from omni.isaac.core import World           #表示场景世界
+from omni.isaac.core.robots import Robot    #表示机器人
+from omni.isaac.core.utils.types import ArticulationAction  #表示关节动作
 
-# open stage
+# open stage，用Omniverse SDK中的usd模块打开名为"simple_case.usd"的USD场景文件。
 omni.usd.get_context().open_stage("simple_case.usd")
 
-# wait two frames so that stage starts loading
+# wait two frames so that stage starts loading，更新模拟应用程序状态
 simulation_app.update()
 simulation_app.update()
 
+#打印提示消息并导入函数检查场景是否正在加载
 print("Loading stage...")
 from omni.isaac.core.utils.stage import is_stage_loading
 
@@ -42,8 +44,8 @@ while is_stage_loading():
     simulation_app.update()
 print("Loading Complete")
 
-world = World(stage_units_in_meters=1.0)
-robot = world.scene.add(Robot(prim_path="/World/panda", name="robot"))
+world = World(stage_units_in_meters=1.0)    #创建了一个名为 world 的 World 实例，设置单位长度为1米
+robot = world.scene.add(Robot(prim_path="/World/panda", name="robot"))  #向场景中添加了一个名为 "panda" 的机器人
 
 world.reset()
 
@@ -55,7 +57,7 @@ while simulation_app.is_running():
         if world.current_time_step_index == 0:
             world.reset()
 
-        # apply actions
+        # apply actions，为机器人应用一个随机的关节动作。
         robot.get_articulation_controller().apply_action(
             ArticulationAction(joint_positions=np.random.random(9))
         )
